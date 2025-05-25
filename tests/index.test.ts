63fa01e6b772c0df76362973d980804b81283c5d
Rin Iwai <rin@eyrin.jp>
2025-05-25 15:55:00 +0900

    todo: use jsdom

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,26 @@
-import { expect, test } from 'vitest';
+import { expect, test, describe } from 'vitest';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+import { scraper } from '../src/browser/scraper';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+describe('getSubject', () => {
+  test('getTimetable returns correct object', () => {
+    const html = readFileSync(
+      path.join(import.meta.dirname, './test0.html'),
+      'utf-8',
+    );
+    document.body.innerHTML = html;
+
+    const timetable = scraper();
+    expect(timetable).toEqual(
+      expect.objectContaining({
+        year: expect.any(String),
+        belong: expect.any(String),
+        semester: expect.any(String),
+        firstHalf: expect.any(Array),
+        secondHalf: expect.any(Array),
+        intensive: expect.any(Array),
+      }),
+    );
+  });
 });
