bcd39d269e18165d9540c9901b952904d8e49c52
Rin Iwai <rin@eyrin.jp>
2025-05-28 20:57:12 +0900

    v0.0.8

--- browser/index.ts
+++ browser/index.ts
@@ -11,39 +11,10 @@ declare global {
 
 (() => {
   try {
-    if (
-      location.href ===
-      'https://kyomu.office.tut.ac.jp/portal/StudentApp/Top.aspx'
-    ) {
-      window.ReactNativeWebView?.postMessage(
-        JSON.stringify({ type: 'loading' } satisfies WebViewMessage),
-      );
-      (
-        document.querySelector<HTMLAnchorElement>(
-          '#ctl00_bhHeader_ctl350_lnk',
-        ) ??
-        document.querySelector<HTMLAnchorElement>('#ctl00_bhHeader_ctl33_lnk')
-      )?.click();
-    }
-
-    if (
-      location.href ===
-      'https://kyomu.office.tut.ac.jp/portal/StudentApp/Blank.aspx#regist_results'
-    ) {
-      document
-        .querySelector<HTMLAnchorElement>('#ctl00_bhHeader_ctl47_lnk')
-        ?.click();
-    }
-
-    if (
-      location.href ===
-      'https://kyomu.office.tut.ac.jp/portal/StudentApp/Regist/RegistList.aspx'
-    ) {
-      const data = getDreamCampusTimetable();
-      window.ReactNativeWebView?.postMessage(
-        JSON.stringify({ type: 'successful', data } satisfies WebViewMessage),
-      );
-    }
+    const data = getDreamCampusTimetable();
+    window.ReactNativeWebView?.postMessage(
+      JSON.stringify({ type: 'successful', data } satisfies WebViewMessage),
+    );
   } catch {
     window.ReactNativeWebView?.postMessage(
       JSON.stringify({ type: 'failed' } satisfies WebViewMessage),
